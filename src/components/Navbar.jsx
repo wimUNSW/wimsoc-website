4b630ed8d154eff8d3dd70dee01e19b5d0b4aa4b
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2025-06-26 22:08:43 +1000

    Made wim logo clickable to go to home page, removed Home link from nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,9 @@ const Navbar = () => {
   return (
     <header className='w-full py-5 fixed top-0 left-0 z-50 bg-black backdrop-blur-md border-b border-gray-200'>
       <nav className='w-full flex max-w-7xl mx-auto px-6 sm:px-8 lg:px-12'>
-        <img src='/assets/WIMBanner.png' alt='wim logo' className='w-45 h-8 object-contain' />
+        <Link to='/'>
+          <img src='/assets/WIMBanner.png' alt='wim logo' className='w-45 h-8 object-contain' />
+        </Link>
 
         <div className='flex flex-1 max-sm:hidden items-center justify-end'>
           {navLists.map((nav) => (
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,6 @@
-// export const navLists = ["Home", "Events", "Sponsors", "About Us", "Contact Us"];
-
 export const navLists = [
-  { item: 'Home', path: '/' },
+  { item: 'About Us', path: '/about-us' },
   { item: 'Events', path: '/events' },
   { item: 'Sponsors', path: '/sponsors' },
-  { item: 'About Us', path: '/about-us' },
   { item: 'Contact Us', path: '/contact' },
 ];
