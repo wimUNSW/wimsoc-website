2100bafadccd850f9e508e651df614586a26c472
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2025-06-27 18:13:31 +1000

    merged css into index.css, made nav disappear on scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,21 +2,32 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className='relative h-screen w-full flex items-center justify-center bg-white px-5' >
-      <div className='w-full flex flex-row justify-center items-center'>
-        <div className='w-1/2 flex-col items-start text-center'>
-          <p id='home' className='home-title text-7xl text-left'>
-            UNSW <br />
-            WOMEN IN <br />
-            MANAGEMENT
-          </p>
-          <p id='tagline' className='text-left mt-4'>Your voice, your vision, your leadership.</p>
+    <>
+      <section className='relative w-full h-full flex items-center justify-center bg-white px-5 py-25' >
+        <div className='w-full flex flex-row justify-center items-center'>
+          <div className='w-1/2 flex-col items-start text-center'>
+            <p id='home' className='home-title text-[5.6rem]/25 text-left'>
+              UNSW <br />
+              WOMEN IN <br />
+              MANAGEMENT
+            </p>
+            <p id='tagline' className='text-left mt-4'>Your voice, your vision, your leadership.</p>
+          </div>
+          <div className='w-1/2 h-full'>
+            <p className='home-title'>other</p>
+          </div>
         </div>
-        <div className='w-1/2 h-full'>
-          <p className='home-title'>other</p>
-        </div>
-      </div>
-    </section>
+      </section>
+
+      <section className='px-5'>
+        <div>hello world</div>
+      </section>
+      <section className='px-5'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
+      </section>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,75 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { navLists } from '../constants';
+
+// const Navbar = () => {
+//   return (
+//     <header className='w-full py-5 fixed top-0 left-0 z-50 bg-black backdrop-blur-md border-b border-gray-200'>
+//       <nav className='w-full flex max-w-7xl mx-auto px-6 sm:px-8 lg:px-12'>
+//         <Link to='/'>
+//           <img src='/assets/WIMBanner.png' alt='wim logo' className='w-45 h-8 object-contain' />
+//         </Link>
+
+//         <div className='flex flex-1 max-sm:hidden items-center justify-end'>
+//           {navLists.map((nav) => (
+//             <Link
+//               to={nav.path}
+//               key={nav.item}
+//               className='px-5 text-sm cursor-pointer hover:text-red-400 transition-all text-white'
+//             >
+//               {nav.item}
+//             </Link>
+//           ))}
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Navbar;
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@heroui/react";
 import { navLists } from '../constants';
 
-const Navbar = () => {
+const NavbarComponent = () => {
   return (
-    <header className='w-full py-5 fixed top-0 left-0 z-50 bg-black backdrop-blur-md border-b border-gray-200'>
-      <nav className='w-full flex max-w-7xl mx-auto px-6 sm:px-8 lg:px-12'>
-        <Link to='/'>
-          <img src='/assets/WIMBanner.png' alt='wim logo' className='w-45 h-8 object-contain' />
-        </Link>
+    <Navbar 
+      shouldHideOnScroll
+      className="backdrop-blur-md border-b border-gray-200 w-full py-5 fixed top-0 left-0 z-50 bg-black border-b"
+      classNames={{
+        base: "py-2",
+        wrapper: "max-w-7xl px-6 sm:px-8 lg:px-12",
+      }}
+    >
+      <NavbarBrand>
+        <RouterLink to='/'>
+          <img 
+            src='/assets/WIMBanner.png' 
+            alt='wim logo' 
+            className='w-45 h-8 object-contain' 
+          />
+        </RouterLink>
+      </NavbarBrand>
+      
+      {/* <NavbarContent className="hidden sm:flex gap-4" justify="end"> */}
+      <NavbarContent className="hidden sm:flex gap-4 absolute left-1/2 transform -translate-x-1/2" justify="center">
 
-        <div className='flex flex-1 max-sm:hidden items-center justify-end'>
-          {navLists.map((nav) => (
-            <Link
+        {navLists.map((nav) => (
+          <NavbarItem key={nav.item}>
+            <Link 
+              as={RouterLink}
               to={nav.path}
-              key={nav.item}
-              className='px-5 text-sm cursor-pointer hover:text-red-400 transition-all text-white'
+              className="px-5 text-lg cursor-pointer hover:text-red-400 transition-all text-white"
             >
               {nav.item}
             </Link>
-          ))}
-        </div>
-      </nav>
-    </header>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavbarComponent;
