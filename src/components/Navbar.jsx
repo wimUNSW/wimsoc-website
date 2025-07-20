14b26787e7b75b0475f064dca4c797a8589ad0ad
Haleemah Rahman <haleeemahrahman@gmail.com>
2025-07-20 10:15:45 +1000

    Upadted navbar to not use hero ui components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link } from "@heroui/react";
 import { navLists } from '../constants';
 
 const NavbarComponent = () => {
   return (
-    <Navbar 
-      shouldHideOnScroll
-      className="w-full py-5 fixed top-0 left-0 z-50 bg-black border-b"
-    >
-      <NavbarBrand>
-        <RouterLink to='/'>
-          <img 
-            src='/assets/WIMBanner.png' 
-            alt='wim logo' 
-            className='w-45 h-8 object-contain' 
-          />
-        </RouterLink>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4 absolute left-1/2 transform -translate-x-1/2" justify="end">
-
-        {navLists.map((nav) => (
-          <NavbarItem key={nav.item}>
-            <Link 
-              as={RouterLink}
+    <nav className="w-full py-5 fixed top-0 left-0 z-50 bg-black border-b">
+      <div className="flex items-center justify-between px-4">
+        <div>
+          <RouterLink to='/'>
+            <img 
+              src='/assets/WIMBanner.png' 
+              alt='wim logo' 
+              className='w-45 h-8 object-contain' 
+            />
+          </RouterLink>
+        </div>
+        
+        <div className="hidden sm:flex gap-4">
+          {navLists.map((nav) => (
+            <RouterLink
+              key={nav.item}
               to={nav.path}
               className="px-5 text-md cursor-pointer hover:text-red-400 transition-all text-white"
             >
               {nav.item}
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
-    </Navbar>
+            </RouterLink>
+          ))}
+        </div>
+      </div>
+    </nav>
   );
 };
 
