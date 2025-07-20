4ed7ad6cd710c89ace7b9587c9203170009674bb
Haleemah Rahman <haleeemahrahman@gmail.com>
2025-07-20 10:17:51 +1000

    Removed extra line

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Theme accentColor="red">
