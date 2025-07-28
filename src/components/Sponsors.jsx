14e6288e9e4ed6aa0bb85b617bf1462944c0f0e7
Haleemah Rahman <haleeemahrahman@gmail.com>
2025-07-28 16:28:46 +1000

    Added heading and website text to sponsors page

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Sponsors = () => {
   return (
-    <div className='bg-white h-screen'>Sponsors</div>
+    <div className='bg-white h-screen flex flex-col items-center justify-start pt-10'>
+      <h1 className='font-[DMSerif] text-7xl text-center mb-8'>Sponsors</h1>
+      <div className='max-w-4xl text-center px-4'>
+        <p className='mb-4'>
+          WIM is currently looking for sponsors to support our mission. Our 2024 
+          Prospectus outlines what our society has to offer including detail into 
+          our sponsorship plans.
+        </p>
+        <p className='mb-4'>
+          If your organisation is interested in becoming a sponsor, contact us 
+          at unsw.wim.industry@gmail.com. 
+        </p>
+      </div>
+    </div>
   )
 }
 
