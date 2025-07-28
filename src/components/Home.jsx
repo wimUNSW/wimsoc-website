d2910ae51b9facbb8170aa480558553887eeda74
Haleemah Rahman <haleeemahrahman@gmail.com>
2025-07-28 16:05:28 +1000

    added font mielle script

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,28 +4,19 @@ const Home = () => {
   return (
     <>
       <section className='relative w-full h-screen bg-white flex pl-20' >
-        <div className='w-full h-100vh flex flex-row'>
+        <div className='w-full flex flex-row'>
           <div className='w-3/4 h-full flex flex-col justify-center items-start'>
-            <p id='home' className='home-title text-8xl text-left'>
+            <p className='home-title text-8xl text-left'>
               UNSW <br />
               WOMEN IN <br />
               MANAGEMENT
             </p>
-            <p id='tagline' className='text-left mt-4'>Your voice, your vision, your leadership.</p>
+            <p className='text-left mt-4 text-4xl tag-line'>Your voice, your vision, your leadership.</p>
           </div>
           <div className='w-1/4 h-full flex justify-center items-center'>
             <p className='home-title'>other</p>
           </div>
         </div>
-      </section>
-
-      <section className='px-5 bg-white'>
-        <div>hello world</div>
-      </section>
-      <section className='px-5 bg-white'>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsum, tempore ex, amet facere repellat eaque odit officiis aliquid sapiente ut! Quo, deserunt? Aliquam magni perferendis, architecto cum similique voluptatum omnis amet maiores dolor debitis impedit rerum est? Optio quam maxime numquam officiis vero doloremque cupiditate, dolorem dolore, neque natus tempore reprehenderit a quia velit minus aliquam corrupti culpa voluptatibus earum ipsam iste impedit. Quia nostrum veritatis mollitia doloremque totam similique dolore velit itaque vel perspiciatis reprehenderit minima necessitatibus error vero molestias, ut aperiam nihil quam delectus officia fuga facere laudantium. Odit optio aliquam sequi quos doloremque laboriosam ipsam nobis!</p>
       </section>
     </>
   )
