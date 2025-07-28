9f090fbf4826e889bb16d9c5e1849635902337c9
Haleemah Rahman <haleeemahrahman@gmail.com>
2025-07-28 16:49:13 +1000

    Styled header

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='bg-white h-screen'>Contact Us</div>
+    <div className='bg-white h-screen flex flex-col items-center justify-start pt-10'>
+      <h1 className='font-[DMSerif] text-7xl text-center mb-8'>Contact Us</h1>
+    </div>
   )
 }
 
